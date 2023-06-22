6e42e3f34aa7ca2cc72b6465b1fefc324a3b77d7
ar-riifat <arrifat40@gmail.com>
2023-06-23 01:20:24 +0600

    commit

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+
+import {AppBar,Toolbar} from '@mui/material';
+const Header  = ()=> {
+    return(
+        <AppBar>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+        
+    )
+}
+
+export default Header;
