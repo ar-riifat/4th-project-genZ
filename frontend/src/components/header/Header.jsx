5fef63151a0fe0bad6572f13b777d64a32321810
ar-riifat <arrifat40@gmail.com>
2023-06-23 22:23:34 +0600

    commit

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,19 +1,39 @@
 
-import {AppBar,Toolbar} from '@mui/material';
-const Header  = ()=> {
+
+import { AppBar, Box, Typography, Toolbar, styled} from '@mui/material';
+
+const styledHeader = styled(AppBar)`
+     background: "#2874f0";
+     height: 55px;
+     `;
     
+const Component = styled(Box)`
+    margin-left: 12%;
+    line-height: 0;
+    `
+
+const SubHeading = styled(Typography)`
+    font-size: 10px;
+    font-style: italic;
+`
+
+const Header  = ()=> {
+
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png'; 
+
     return(
         <styleHeader>
             <Toolbar>
-                <component>
-                    <img src="picture.jpg" alt="logo" style={{ width: 75}} />
+                <Component>
+                    <img src={logoURL} alt="logo" style={{ width: 75}} />
                     <box>
-                        <subHeding>Explore&nbsp;
+                        <subHeading>Explore&nbsp;
                             <Box component="span" style={{ color: '#FFE500'}}>Plus</Box>
-                        </subHeding>
-                        <img src=" " alt="" />
+                        </subHeading>
+                        <img src={subURL} alt="sub-logo" />
                     </box>
-                </component>
+                </Component>
             </Toolbar>
         </styleHeader>
         
