0e596c748c579415695aab32805c565d614ada7c
ar-riifat <arrifat40@gmail.com>
2023-06-25 01:28:52 +0600

    commit

---
+++ backend/default.js
@@ -0,0 +1,18 @@
+
+import { products } from "./constants/data.js"
+import Product from "./model/product-schema.js";
+
+const DefaultData =  async () => {
+    try {
+        // await Product.deleteMany({});
+        await Product.insertMany(products);
+
+        console.log('Data imported Successfully');
+
+    } catch (error) {
+        console.log('Error while inserting default data', error.message);
+    }
+
+}
+
+export default DefaultData;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import dotenv from 'ditenv';
+import dotenv from 'dotenv';
 
 import Connection from './database/db.js';
-
+import DefaultData from './default.js';
 
 const app = express();
 
@@ -14,7 +14,12 @@ const PORT = 8000
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
+Connection( USERNAME,PASSWORD);
 
+app.get("/", (req,res) => {
+    res.send("welcome to home page ");
+  });
 
 app.listen(PORT, () => console.log (`Server Is running successfully on PORT ${PORT} Hello`));
-Connection( USERNAME,PASSWORD);
+
+DefaultData();
