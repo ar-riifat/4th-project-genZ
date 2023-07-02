80e76ad91cf4fd59580916bcfa962310a7cc9b55
ar-riifat <arrifat40@gmail.com>
2023-07-03 01:16:25 +0600

    commit

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 
 import Connection from './database/db.js';
 import DefaultData from './default.js';
+import Router from './routes/route.js';
 
 const app = express();
 
 dotenv.config();
 
+app.use('/', Router);
 
 const PORT = 8000
 
